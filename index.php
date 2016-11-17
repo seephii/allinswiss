fe1e1f9c35fa620972dc2d8074d47547f7a8d745
Jakob Spörri <jakob.spoerri@mmp.htwchur.ch>
2016-11-17 14:54:20 +0100

    jakob

--- index.php
+++ index.php
@@ -33,7 +33,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="#">All-in-Swiss</a>
+      <a class="navbar-brand" href="#">All-in-Swiss-Jakob</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
