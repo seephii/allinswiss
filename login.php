854b7a0df762fac477d3ac139ab01735f393e6fe
Stephanie Fuhrer <stephanie.fuhrer@mmp.htwchur.ch>
2016-11-17 16:00:14 +0100

    diverse Änderungen

--- login.php
+++ login.php
@@ -12,7 +12,6 @@
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/custom.css" rel="stylesheet">
 
-
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
@@ -21,7 +20,10 @@
     <![endif]-->
   </head>
   <body>
-    <h1>Melden Sie sich bitte an!</h1>
+    <div class="zentriert">
+      <h2>Melden Sie sich bitte an!</h2>
+    </div>
+    <br>
 
     <div class="container">
           <div class="row">
@@ -97,7 +99,8 @@
           </div>
         </div>
       </div>
-      <br>
+
+
 
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
