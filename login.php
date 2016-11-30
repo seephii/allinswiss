3aa09a9579ca87e418a4a4b1155dd67a3307025a
Jakob Spörri <jakob.spoerri@mmp.htwchur.ch>
2016-11-30 10:13:40 +0100

    login von Bock übernommen

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
   session_start();
-    if(isset($_SESSION["id"])) unset($_SESSION["id"]);
-    session_destroy();
+  if(isset($_SESSION["id"])) unset($_SESSION["id"]);
+  session_destroy();
 
-    require_once('System/data.php');
-    require_once('System/security.php');
+  require_once('System/data.php');
+  require_once('System/security.php');
 
   $error = false;
   $error_msg = "";
@@ -61,113 +61,137 @@
     }
   }
  ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Login</title><br>
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap-theme.css" rel="stylesheet">
-    <link href="css/bootstrap.css" rel="stylesheet">
-    <link href="css/custom.css" rel="stylesheet">
+    <title>Login Allinswiss</title>
 
+    <!-- Bootstrap core CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <!-- Custom styles for this template -->
+    <link href="css/login.css" rel="stylesheet">
   </head>
+
   <body>
-    <h1>Melden Sie sich bitte an!</h1>
-
+    <!-- http://bootsnipp.com/snippets/featured/login-and-register-tabbed-form -->
     <div class="container">
-          <div class="row">
-          <div class="col-md-6 col-md-offset-3">
-            <div class="panel panel-login">
-              <div class="panel-heading">
-                <div class="row">
-                  <div class="col-xs-6">
-                    <a href="#" class="active" id="login-form-link">Login</a>
-                  </div>
-                  <div class="col-xs-6">
-                    <a href="#" id="register-form-link">Register</a>
-                  </div>
-                </div>
-                <hr>
-              </div>
-              <div class="panel-body">
-                <div class="row">
-                  <div class="col-lg-12">
-                    <form id="login-form" action="http://phpoll.com/login/process" method="post" role="form" style="display: block;">
-                      <div class="form-group">
-                        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
-                      </div>
-                      <div class="form-group">
-                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-                      </div>
-                      <div class="form-group text-center">
-                        <input type="checkbox" tabindex="3" class="" name="remember" id="remember">
-                        <label for="remember">Remember Me</label>
-                      </div>
-                      <div class="form-group">
-                        <div class="row">
-                          <div class="col-sm-6 col-sm-offset-3">
-                            <input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="Log In">
-                          </div>
-                        </div>
-                      </div>
-                      <div class="form-group">
-                        <div class="row">
-                          <div class="col-lg-12">
-                            <div class="text-center">
-                              <a href="http://phpoll.com/recover" tabindex="5" class="forgot-password">Forgot Password?</a>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                    <form id="register-form" action="http://phpoll.com/register/process" method="post" role="form" style="display: none;">
-                      <div class="form-group">
-                        <input type="text" name="username" id="username" tabindex="1" class="form-control" placeholder="Username" value="">
-                      </div>
-                      <div class="form-group">
-                        <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Email Address" value="">
-                      </div>
-                      <div class="form-group">
-                        <input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Password">
-                      </div>
-                      <div class="form-group">
-                        <input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Confirm Password">
-                      </div>
-                      <div class="form-group">
-                        <div class="row">
-                          <div class="col-sm-6 col-sm-offset-3">
-                            <input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="Register Now">
-                          </div>
-                        </div>
-                      </div>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      <br>
+    	<div class="row">
+  			<div class="col-md-6 col-md-offset-3">
+  				<div class="panel panel-login">
+  					<div class="panel-heading">
+  						<div class="row">
+  							<div class="col-xs-12">
+    							<h3> p42</h3>
+  							</div>
+  						</div>
 
+  						<div class="row">
+  							<div class="col-xs-6">
+  								<a href="#" class="active" id="login-form-link">Login</a>
+  							</div>
+  							<div class="col-xs-6">
+  								<a href="#" id="register-form-link">anmelden</a>
+  							</div>
+  						</div>
+  						<hr>
+  					</div>
+  					<div class="panel-body">
+  						<div class="row">
+  							<div class="col-lg-12">
+  								<!-- Login-Formular -->
+  								<form id="login-form" action="index.php" method="post" role="form" style="display: block;">
+  									<div class="form-group">
+  										<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="E-Mail-Adresse" value="">
+  									</div>
+  									<div class="form-group">
+  										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Passwort">
+  									</div>
+  									<div class="form-group">
+  										<div class="row">
+  											<div class="col-sm-6 col-sm-offset-3">
+  												<input type="submit" name="login-submit" id="login-submit" tabindex="4" class="form-control btn btn-login" value="einloggen">
+  											</div>
+  										</div>
+  									</div>
+  								</form>
+  								<!-- /Login-Formular -->
+
+  								<form id="register-form" action="index.php" method="post" role="form" style="display: none;">
+  									<div class="form-group">
+  										<input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="E-Mail-Adresse" value="">
+  									</div>
+  									<div class="form-group">
+  										<input type="password" name="password" id="password" tabindex="2" class="form-control" placeholder="Passwort">
+  									</div>
+  									<div class="form-group">
+  										<input type="password" name="confirm-password" id="confirm-password" tabindex="2" class="form-control" placeholder="Passwort bestätigen">
+  									</div>
+  									<div class="form-group">
+  										<div class="row">
+  											<div class="col-sm-6 col-sm-offset-3">
+  												<input type="submit" name="register-submit" id="register-submit" tabindex="4" class="form-control btn btn-register" value="jetzt registrieren">
+  											</div>
+  										</div>
+  									</div>
+  								</form>
+
+  							</div>
+  						</div>
+  					</div>
+  				</div>
+  			</div>
+  		</div>
+  		<!-- Die beiden BS-Allert-Boxen geben Rückmeldung über den Registrierungsprozess. Sie werden erst später benötigt. -->
+<?php
+  if($success == true){
+?>
+      <div class="alert alert-success" role="alert">Es hat funktioniert.</div>
+<?php
+  }
+?>
+<?php
+  if($error == true){
+?>
+      <div class="alert alert-danger" role="alert"><?php echo $error_msg; ?></div>
+<?php
+  }
+?>
+
+      </div><!-- /container -->
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+    <script>
+      $(function() {
+
+        $('#login-form-link').click(function(e) {
+      		$("#login-form").delay(100).fadeIn(100);
+       		$("#register-form").fadeOut(100);
+      		$('#register-form-link').removeClass('active');
+      		$(this).addClass('active');
+      		e.preventDefault();
+      	});
+
+      	$('#register-form-link').click(function(e) {
+      		$("#register-form").delay(100).fadeIn(100);
+       		$("#login-form").fadeOut(100);
+      		$('#login-form-link').removeClass('active');
+      		$(this).addClass('active');
+      		e.preventDefault();
+      	});
+
+      });
+    </script>
+
   </body>
 </html>
