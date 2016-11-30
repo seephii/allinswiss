55b1687132201af4abee8444f663f16f478b69c6
Jakob Spörri <jakob.spoerri@mmp.htwchur.ch>
2016-11-30 15:46:30 +0100

    no message

--- benutzer.php
+++ benutzer.php
@@ -1,13 +1,11 @@
 <?php
-session_start();
-if(!isset($_SESSION['id'])){
-    header ("Location:index.php");
-}else{
-  $user_id = $_SESSION['id'];
-}
-
-?>
-
+  session_start();
+  if(!isset($_SESSION['id'])){
+    header("Location:index.php");
+  }else{
+    $user_id = $_SESSION['id'];
+  }
+  ?>
 
 <!DOCTYPE html>
 <html lang="en">
