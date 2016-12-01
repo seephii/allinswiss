f66296f40e55daf3fa5952e8e7fd7eea3f7845cb
meyerbianca <bianca.meyer@mmp.htwchur.ch>
2016-12-01 13:56:17 +0100

    nicht wichtig

--- auswahl.php
+++ auswahl.php
@@ -14,15 +14,15 @@ session_start();
   if(isset($_GET['hotelauswahl-submit'])){
         $sterne = filter_data($_GET['sterne']); // dieses filter_data muss nun überall eingefügt werden. (also überall bei $POST?)
         $ortschaft = filter_data($_GET['id_ort']);
-        $dienstleister = filter_data($_GET['id_dienstleister']);
+        $aktivitaet = filter_data($_GET['id_aktivitaet']);
 
         $hotel_result = hotelauswahl($sterne, $ortschaft);
-        $dienstleister_result = dienstleisterauswahl($dienstleister);
+        $aktivitaet_result = aktivitaetauswahl($aktivitaet);
+      } else{
+        $error = true;
+        $error_msg .= "Leider konnten wir kein Hotel zu diesen Bedingungen finden. <br/>";
+       }
 
-        $row_count = mysqli_num_rows($hotel_result);
-
-
-     }
 
 ?>
 
@@ -35,6 +35,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <title>Auswahl</title>
+<p> Hallo Velo - das ist ein Test - ob Biancas Github funktioniert</p>
 
     <!-- Bootstrap -->
     <link href="css/custom.css" rel="stylesheet">
@@ -103,14 +104,10 @@ session_start();
           <div class="funkyradio-default">
             <input type="checkbox" name="hotels[]" id="hotel-<?php $row["id_hotel"]; ?>" />
           <!-- Option kommt in die Schlaufe, damit alles untereinander angezeigt wird (nur Echo in Option)-->
-            <label for="hotel-<?php $row["id_hotel"];?>"><?php echo ($row["hotelname"]);
-            ?></label>
+            <label for="hotel-<?php $row["id_hotel"]; ?>"><?php echo ($row["hotelname"]); ?></label>
           </div>
           <?php
           }
-          if($row_count == 0){
-          echo "Leider konnten wir kein Hotel zu diesen Bedingungen finden. <br/>";
-         }
           ?>
     </div>
 </div>
@@ -122,7 +119,7 @@ session_start();
 
            <?php
            /*Schlaufe, damit alle Sterne abgefragt werden*/
-           while($row = mysqli_fetch_assoc($dienstleister_result))
+           while($row = mysqli_fetch_assoc($aktivitaet_result))
            {
            ?>
            <div class="funkyradio-default">
@@ -131,10 +128,7 @@ session_start();
              <label for="aktivitaet-<?php $row["id_aktivitaet"]; ?>"><?php echo ($row["aktivitaet"]); ?></label>
            </div>
            <?php
-         }
-         if($row_count == 0){
-         echo "Leider konnten wir keine Aktivitäten zu diesen Bedingungen finden. <br/>";
-        }
+           }
            ?>
      </div>
 </div>
